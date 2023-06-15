c5fd87954b281c7f87a9bdeb75bbe3a7621de54f
Saad Tariq <Saad.Tariq@tpsonline.com>
2023-06-15 12:56:59 +0500

    Initial setup commit

---
+++ FoodShare/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodShare.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
